5c66c46451757fb3c5b35bc730b7a7ea10fb8365
Eric Hartman <imerichartman@gmail.com>
2016-11-24 11:28:50 -0500

    Turned into Kurt vs Kermit instead of Red/Blue

--- tictac/js/main.js
+++ tictac/js/main.js
@@ -2,49 +2,49 @@ $(function(){
 
 function check(){
     if ($('#one').hasClass("red") && $('#two').hasClass("red") && $('#three').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#four').hasClass("red") && $('#five').hasClass("red") && $('#six').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#seven').hasClass("red") && $('#eight').hasClass("red") && $('#nine').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#one').hasClass("red") && $('#four').hasClass("red") && $('#seven').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#two').hasClass("red") && $('#five').hasClass("red") && $('#eight').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#three').hasClass("red") && $('#six').hasClass("red") && $('#nine').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#one').hasClass("red") && $('#five').hasClass("red") && $('#nine').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#three').hasClass("red") && $('#five').hasClass("red") && $('#seven').hasClass("red")) {
-      alert("Red Wins");
+      alert("Kurt Wins");
     } else if ($('#one').hasClass("blue") && $('#two').hasClass("blue") && $('#three').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#four').hasClass("blue") && $('#five').hasClass("blue") && $('#six').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#seven').hasClass("blue") && $('#eight').hasClass("blue") && $('#nine').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#one').hasClass("blue") && $('#four').hasClass("blue") && $('#seven').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#two').hasClass("blue") && $('#five').hasClass("blue") && $('#eight').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#three').hasClass("blue") && $('#six').hasClass("blue") && $('#nine').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#one').hasClass("blue") && $('#five').hasClass("blue") && $('#nine').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     } else if ($('#three').hasClass("blue") && $('#five').hasClass("blue") && $('#seven').hasClass("blue")) {
-      alert("Blue Wins");
+      alert("Kermit Wins");
     }
   }
 
-  var moves = 0
+  var moves = 0;
   $('.board div').on('click', function() {
     moves++;
     console.log(moves);
-    if (moves % 2 === 1) {
-    $(this).attr('class', 'blue');
+    if (moves % 2 === 1 ) {
+    $(this).attr('class', 'red');
     check()
   } else
-    $(this).attr('class', 'red');
+    $(this).attr('class', 'blue');
     check()
   });
 
