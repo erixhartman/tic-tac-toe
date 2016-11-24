f8ae7a67c6c38bdf151bb864be8b41151d1b798e
Eric Hartman <imerichartman@gmail.com>
2016-11-24 14:40:37 -0500

    Now cannot click on clicked square

--- tictac/js/main.js
+++ tictac/js/main.js
@@ -38,14 +38,16 @@ function check(){
 
   var moves = 0;
   $('.board div').on('click', function() {
-    moves++;
     console.log(moves);
-    if (moves % 2 === 1 ) {
+    if (moves % 2 === 1 && (!$(this).hasClass('red')) && (!$(this).hasClass('blue'))) {
+    moves++;
     $(this).attr('class', 'red');
     check()
-  } else
+  } else if (moves % 2 === 0 && (!$(this).hasClass('red')) && (!$(this).hasClass('blue'))) {
+    moves++;
     $(this).attr('class', 'blue');
     check()
+  }
   });
 
   $('button').on('click', function() {
